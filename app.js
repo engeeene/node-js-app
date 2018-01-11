b56ea4a5e25e31b98f674cd87f3ce266f75ae97c
engeeene <en9eeene@gmail.com>
2018-01-12 00:37:41 +0200

    ~ change '/' test ~ text changing

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 	
 app.get("/", function(request, response){
 	response.render('home.hbs',{
-		title: 'test'
+		title: 'node-js-app'
 	});
 });
 
